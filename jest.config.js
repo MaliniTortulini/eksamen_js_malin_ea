e0e7bc29cffadb5fc09a13d4d668ccb1ae290b59
Malinitortulini <malin.eftedal@gmail.com>
2024-12-05 16:47:38 +0100

    config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    globals: {
+      'ts-jest': {
+        tsconfig: 'tsconfig.json',
+      },
+    },
+    transform: {
+      "^.+.tsx?$": ["ts-jest",{}],
+    },
+  };
